e1bc042e4f7250ed037a7bec23fa9eed14789f5f
edwin vakkachan <edwinvakkachan@gmail.com>
2026-02-24 11:50:42 +0530

    changes in error

---
+++ timelog.js
@@ -0,0 +1,13 @@
+import { sendMessage } from "./telegram/sendTelegramMessage.js";
+
+
+
+export async function log(message='⌚') {
+  const time = new Date().toLocaleString('en-IN', {
+    timeZone: 'Asia/Kolkata',
+    hour12: false
+  });
+
+await sendMessage(`[${time}] ${message}`)
+  console.log(`[${time}] ${message}`);
+}
